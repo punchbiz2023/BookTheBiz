270f4e68b52bfe171e43eceea3e7d9c505a293e0
DharshanPrakash <dharshanprakasherode@gmail.com>
2024-09-13 00:56:21 +0530

    Started location updates and started enhanciing the functionalities of the homescreen ui

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,11 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart' as fs;
 import 'package:flutter/material.dart';
-import 'package:odp/pages/profile.dart';
-import 'package:odp/pages/settings.dart';
+import 'package:geolocator/geolocator.dart'; // Import geolocator package
+import 'package:permission_handler/permission_handler.dart'; // Import permission_handler package
 
 import 'bookingpage.dart';
 import 'login.dart';
+import 'profile.dart';
+import 'settings.dart';
 
 class HomePage1 extends StatefulWidget {
   final User? user;
@@ -18,12 +20,16 @@ class HomePage1 extends StatefulWidget {
 }
 
 class _HomePage1State extends State<HomePage1> {
+  final GlobalKey<ScaffoldState> _scaffoldKey =
+      GlobalKey<ScaffoldState>(); // Added GlobalKey
   String userType = 'User'; // Default user type
+  Position? _currentPosition;
 
   @override
   void initState() {
     super.initState();
     _fetchUserType();
+    _checkAndFetchLocation(); // Check permissions and fetch location
   }
 
   Future<void> _fetchUserType() async {
@@ -34,15 +40,7 @@ class _HomePage1State extends State<HomePage1> {
             .doc(widget.user!.uid)
             .get();
 
-        print('Document data: ${userDoc.data()}');
-
-        // Print the entire document data to check its contents
-        print('Fetched user document: ${userDoc.data()}');
-
-        // Fetch and print the userType field
         String fetchedUserType = userDoc.get('userType') ?? 'User';
-        print('Fetched userType: $fetchedUserType');
-
         setState(() {
           userType = fetchedUserType;
         });
@@ -52,25 +50,59 @@ class _HomePage1State extends State<HomePage1> {
     }
   }
 
+  Future<void> _checkAndFetchLocation() async {
+    if (await Permission.location.request().isGranted) {
+      _fetchCurrentLocation();
+    } else {
+      // Handle the case where permission is not granted
+      print('Location permission not granted');
+    }
+  }
+
+  Future<void> _fetchCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      setState(() {
+        _currentPosition = position;
+      });
+    } catch (e) {
+      print('Error fetching location: $e');
+    }
+  }
+
+  Future<void> _logout() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginApp()),
+      );
+    } catch (e) {
+      print('Error logging out: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey, // Use the GlobalKey
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          'Booking App',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        leading: Builder(
-          builder: (context) => IconButton(
-            icon: Icon(Icons.menu, color: Colors.white),
-            onPressed: () {
-              Scaffold.of(context).openDrawer();
-            },
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        flexibleSpace: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: Icon(Icons.menu, color: Colors.white),
+                onPressed: () {
+                  _scaffoldKey.currentState
+                      ?.openDrawer(); // Open drawer using the GlobalKey
+                },
+              ),
+              _buildLocationWidget(),
+            ],
           ),
         ),
         automaticallyImplyLeading: false,
@@ -145,10 +177,7 @@ class _HomePage1State extends State<HomePage1> {
                     icon: Icons.logout,
                     text: 'Logout',
                     onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginApp()),
-                      );
+                      _logout(); // Implement logout functionality
                     },
                   ),
                 ],
@@ -164,7 +193,7 @@ class _HomePage1State extends State<HomePage1> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'Upcoming Events',
+              'Quick Access',
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 22,
@@ -175,16 +204,22 @@ class _HomePage1State extends State<HomePage1> {
             // Display content based on user type
             if (userType == 'User')
               Container(
-                height: 120,
+                height: 250, // Adjust height as needed
                 child: ListView(
                   scrollDirection: Axis.horizontal,
                   children: [
                     FirebaseImageCard(
-                        imagePath: 'Turf images test/turf 1.jpeg'),
+                        imagePath: 'Turf images test/turf 2.jpeg',
+                        title: 'Turf 1',
+                        description: 'Description for Turf 1'),
                     FirebaseImageCard(
-                        imagePath: 'Turf images test/turf 2.jpeg'),
+                        imagePath: 'Turf images test/turf 3.jpeg',
+                        title: 'Turf 2',
+                        description: 'Description for Turf 2'),
                     FirebaseImageCard(
-                        imagePath: 'Turf images test/turf 3.jpeg'),
+                        imagePath: 'Turf images test/turf 4.jpeg',
+                        title: 'Turf 3',
+                        description: 'Description for Turf 3'),
                   ],
                 ),
               )
@@ -220,6 +255,16 @@ class _HomePage1State extends State<HomePage1> {
     );
   }
 
+  Widget _buildLocationWidget() {
+    if (_currentPosition == null) {
+      return CircularProgressIndicator(); // or any placeholder
+    }
+    return Text(
+      '${_currentPosition!.latitude.toStringAsFixed(2)}, ${_currentPosition!.longitude.toStringAsFixed(2)}',
+      style: TextStyle(color: Colors.white, fontSize: 16),
+    );
+  }
+
   Widget _createDrawerItem({
     required IconData icon,
     required String text,
@@ -242,9 +287,15 @@ class _HomePage1State extends State<HomePage1> {
 
 class FirebaseImageCard extends StatefulWidget {
   final String imagePath;
+  final String title;
+  final String description;
 
-  const FirebaseImageCard({Key? key, required this.imagePath})
-      : super(key: key);
+  const FirebaseImageCard({
+    Key? key,
+    required this.imagePath,
+    required this.title,
+    required this.description,
+  }) : super(key: key);
 
   @override
   _FirebaseImageCardState createState() => _FirebaseImageCardState();
@@ -280,20 +331,85 @@ class _FirebaseImageCardState extends State<FirebaseImageCard> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : imageUrl != null
-              ? Image.network(
-                  imageUrl!,
-                  fit: BoxFit.cover,
-                )
-              : Center(
-                  child: Text(
-                    'Failed to load image',
-                    style: TextStyle(color: Colors.red),
-                  ),
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.6, // Adjust width as needed
+      height: MediaQuery.of(context).size.height * 0.75, // 75% of screen height
+      margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
+      child: Card(
+        elevation: 0, // Set elevation to 0 to avoid shadow
+        color: Colors.transparent, // Transparent background
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Stack(
+          children: [
+            // Background image
+            ClipRRect(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+              child: isLoading
+                  ? Center(child: CircularProgressIndicator())
+                  : imageUrl != null
+                      ? Image.network(
+                          imageUrl!,
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        )
+                      : Center(
+                          child: Text(
+                            'Failed to load image',
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ),
+            ),
+            // Overlay for text readability
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: Colors.black54, // Semi-transparent overlay
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.title,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white, // Ensure title is visible
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      widget.description,
+                      style: TextStyle(
+                        color:
+                            Colors.white, // Set description text color to white
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Add your action here
+                      },
+                      child: Text('Book Now'),
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor:
+                            Colors.blueAccent, // Text color on button
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
