59f7193a34aa2f199b2ea7e244b8af0ebfd96695
DharshanPrakash <dharshanprakasherode@gmail.com>
2024-09-16 13:32:17 +0530

    customized details page

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -13,7 +13,6 @@ class DetailsPage extends StatelessWidget {
 
   Future<Map<String, dynamic>?> _fetchDetails() async {
     try {
-      // Fetch the document from Firestore using documentId
       DocumentSnapshot<Map<String, dynamic>> documentSnapshot =
           await FirebaseFirestore.instance
               .collection('turfs') // Replace with your collection
@@ -32,13 +31,87 @@ class DetailsPage extends StatelessWidget {
     }
   }
 
+  IconData _getIconForItem(String item) {
+    switch (item.toLowerCase()) {
+      case 'football field':
+        return Icons.sports_soccer;
+      case 'volleyball court':
+        return Icons.sports_volleyball;
+      case 'cricket ground':
+        return Icons.sports_cricket;
+      case 'basketball court':
+        return Icons.sports_basketball;
+      case 'swimming pool':
+        return Icons.pool;
+      case 'shuttlecock':
+        return Icons.sports_tennis;
+      case 'tennis court':
+        return Icons.sports_tennis;
+      case 'badminton court':
+        return Icons.sports_tennis;
+      case 'parking':
+        return Icons.local_parking;
+      case 'restroom':
+        return Icons.wc;
+      case 'cafeteria':
+        return Icons.restaurant;
+      case 'lighting':
+        return Icons.lightbulb;
+      case 'seating':
+        return Icons.event_seat;
+      case 'shower':
+        return Icons.shower;
+      case 'changing room':
+        return Icons.room_preferences;
+      case 'wi-fi':
+        return Icons.wifi;
+      default:
+        return Icons.sports;
+    }
+  }
+
+  Widget _buildChipList(String title, List<dynamic> items,
+      Color backgroundColor, Color labelColor) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '$title:',
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        SizedBox(height: 8),
+        Wrap(
+          spacing: 6.0,
+          runSpacing: 3.0,
+          children: items
+              .map((item) => Chip(
+                    label: Text(item),
+                    avatar: Icon(
+                      _getIconForItem(item),
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                    backgroundColor: backgroundColor,
+                    labelStyle: TextStyle(
+                      color: labelColor,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ))
+              .toList(),
+        ),
+        SizedBox(height: 16),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(documentname),
-      ),
       body: FutureBuilder<Map<String, dynamic>?>(
+        // Note the removal of the AppBar from here
         future: _fetchDetails(),
         builder: (BuildContext context,
             AsyncSnapshot<Map<String, dynamic>?> snapshot) {
@@ -57,22 +130,19 @@ class DetailsPage extends StatelessWidget {
                 snapshot.data!['availableGrounds'] ?? [];
             List<dynamic> facilities = snapshot.data!['facilities'] ?? [];
 
-            return SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Display the image
-                    imageUrl.isNotEmpty
+            return CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  expandedHeight: 250,
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                    title: Text(documentname),
+                    background: imageUrl.isNotEmpty
                         ? Image.network(
                             imageUrl,
-                            height: 250,
-                            width: double.infinity,
                             fit: BoxFit.cover,
                           )
                         : Container(
-                            height: 250,
                             color: Colors.grey,
                             child: Center(
                               child: Text(
@@ -81,53 +151,35 @@ class DetailsPage extends StatelessWidget {
                               ),
                             ),
                           ),
-                    SizedBox(height: 16),
-
-                    // Display Available Grounds as Chips
-                    Text(
-                      'Available Grounds:',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    Wrap(
-                      spacing: 8.0,
-                      runSpacing: 4.0,
-                      children: availableGrounds
-                          .map((ground) => Chip(
-                                label: Text(ground),
-                                backgroundColor: Colors.blueAccent,
-                                labelStyle: TextStyle(color: Colors.white),
-                              ))
-                          .toList(),
-                    ),
-                    SizedBox(height: 16),
-
-                    // Display Facilities as Chips
-                    Text(
-                      'Facilities:',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    Wrap(
-                      spacing: 8.0,
-                      runSpacing: 4.0,
-                      children: facilities
-                          .map((facility) => Chip(
-                                label: Text(facility),
-                                backgroundColor: Colors.greenAccent,
-                                labelStyle: TextStyle(color: Colors.black),
-                              ))
-                          .toList(),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            _buildChipList(
+                              'Available Grounds',
+                              availableGrounds,
+                              Colors.blueAccent,
+                              Colors.white,
+                            ),
+                            _buildChipList(
+                              'Facilities',
+                              facilities,
+                              Colors.green,
+                              Colors.black,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             );
           }
 
